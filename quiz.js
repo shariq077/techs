785d9e82cc62c31f1ce9da0550d693bf7265e25a
shariq077 <38794210+shariq077@users.noreply.github.com>
2020-06-21 23:40:15 +0500

    Create quiz.js

---
+++ quiz.js
@@ -0,0 +1,93 @@
+// 1. Create a multidimensional array to hold quiz questions and answers
+
+const questionArray = [
+['How many planets are in the Solar System?', '8'],
+['How many continents are there?', '7'],
+['How many legs does an insect have?','4']  
+['What year was JavaScript created?', '1995']
+]
+// 2. Store the number of questions answered correctly
+
+
+/* 
+  3. Use a loop to cycle through each question
+      - Present each question to the user
+      - Compare the user's response to answer in the array
+      - If the response matches the answer, the number of correctly
+        answered questions increments by 1
+*/
+
+
+// 4. Display the number of correct answers to the user
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//         Search-list 
+//const inStock = ['pizza', 'cookies', 'eggs', 'apples', 'milk', 'cheese', 'bread', 'lettuce', 'carrots', 'broccoli', 'potatoes', 'crackers', 'onions', 'tofu', 'limes', 'cucumbers'];
+//let search = prompt('Search for a product.');
+//let productIndex;
+//let message;
+//
+//if (search) {
+//  search = search.toLowerCase();
+//  productIndex = inStock.indexOf(search);
+//}
+//
+//if (!search){
+//  message = `<strong>In Stock: </strong> ${inStock.join(', ')}`;
+//} else if ( productIndex !== -1) {
+//  message = `Yes, we have <strong>${search}</strong>`;
+//} else {
+//  message = `Sorry, we do not have <strong>${search}</strong>`;
+//}
+//
+//document.querySelector('main').innerHTML = `<p>${message}</p>`;
+//
+//const people = [ 'Maria', 'Alyssa', 'Toni', 'Lee', 'Reggie' ];
+//let personIndex = people.indexOf('guil');  // 0
+//
+//if ( personIndex ) { // it's ok because we're checking for -1 only
+// console.log(`${people[personIndex]} is in the array at index ${personIndex}.`);
+//}
+
+//           Playlist - 2
+//const playlist = [
+//  ['So What', ' Miles David'],
+//  ['Respect',' Arethan Franklin'],
+//  ['What a Wonderful World', ' Louis'],
+//  ['At Last', ' Ella'],
+//  ['Three Little Birds', ' Bob Marley'],
+//  ['The Way You Look Tonight', ' Frank Sinatra']
+//];
+//
+//function createListItems( arr ) {
+//  let items = '';
+//  for ( let i = 0; i < arr.length; i++ ) {
+//    items += `<li>${ arr[i][0] }, by ${ arr[i][1]} </li>`;
+//  }
+//  return items;
+//}
+//
+//document.querySelector('main').innerHTML = `<ol>${createListItems(playlist)}</ol>`;
+
